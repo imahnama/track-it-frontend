d0ddb892d01a150879fd178d6815da9f4dc0b262
imahnama <muannama25@gmail.com>
2021-01-15 22:58:58 +0300

    Add routes to main component

---
+++ src/containers/App.js
@@ -0,0 +1,32 @@
+import {
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
+import HomePage from '../components/homepage';
+import Login from '../components/Auth/Login';
+import Signup from '../components/Auth/signup';
+import NavBar from '../components/navbar';
+import Information from '../components/information';
+import CreateActivity from '../components/createActivity';
+import Activities from './Activities';
+import CreateMeasurement from '../components/CreateMeasurement';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/homepage" component={HomePage} />
+          <Route exact path="/Auth/signup" component={Signup} />
+          <Route exact path="/information" component={Information} />
+          <Route exact path="/create-activity" component={CreateActivity} />
+          <Route exact path="/activities" component={Activities} />
+          <Route exact path="/activity/:id/create" component={CreateMeasurement} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
