34988c7ce652d1569876852fba224740620f08e4
imahnama <muannama25@gmail.com>
2021-01-15 22:56:36 +0300

    Write activities actions

---
+++ src/actions/activitiesAction.js
@@ -0,0 +1,58 @@
+import { GetActivityRequest, CreateActivityRequest, DeleteActivityRequest } from '../Utility/api';
+
+export const GET_ACTIVITIES_REQUEST = 'GET_ACTIVITIES_REQUEST';
+export const GET_ACTIVITIES_SUCCESS = 'GET_ACTIVITIES_SUCCESS';
+export const GET_ACTIVITIES_FAILURE = 'GET_ACTIVITIES_FAILURE';
+export const DELETE_ACTIVITY = 'DELETE_ACTIVITY';
+
+export const getActivitiesSuccess = activities => ({
+  type: GET_ACTIVITIES_SUCCESS,
+  payload: activities,
+});
+
+export const getActivitiesRequest = () => ({
+  type: GET_ACTIVITIES_REQUEST,
+});
+
+export const getActivitiesFailure = error => ({
+  type: GET_ACTIVITIES_FAILURE,
+  payload: error,
+});
+
+export const deleteActivityRequest = id => ({
+  type: DELETE_ACTIVITY,
+  payload: id,
+});
+
+export const deleteActivity = id => async dispatch => {
+  const method = 'delete';
+  try {
+    await DeleteActivityRequest(method, id);
+    dispatch(deleteActivityRequest(id));
+  } catch (error) {
+    dispatch(getActivitiesFailure(error));
+  }
+};
+
+export const getActivities = () => async dispatch => {
+  const method = 'get';
+  try {
+    const response = await GetActivityRequest(method);
+    const activities = response.data;
+    dispatch(getActivitiesSuccess(activities));
+  } catch (error) {
+    dispatch(getActivitiesFailure());
+  }
+};
+
+export const createActivity = (form, history) => async dispatch => {
+  history.push('/activities');
+  dispatch(getActivitiesRequest());
+  const method = 'post';
+  try {
+    await CreateActivityRequest(method, form);
+    dispatch(getActivities());
+  } catch (error) {
+    dispatch(getActivitiesFailure());
+  }
+};
