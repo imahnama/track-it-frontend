0e35c953a7fa67ab0da746bdc376f2a4bb4563bd
imahnama <muannama25@gmail.com>
2021-02-08 18:10:15 +0300

    remove unused files

--- src/reducers/activities.js
+++
@@ -1,8 +0,0 @@
-import { ACTIVITIES_ERRORS } from '../actions/types';
-
-export default (state = [], action) => {
-  if (action.type === ACTIVITIES_ERRORS) {
-    return action.errors;
-  }
-  return state;
-};
--- src/reducers/error.js
+++
@@ -1,8 +0,0 @@
-// import { SET_ERRORS } from '../actions/types';
-//
-// export default (state = [], action) => {
-//   if (action.type === SET_ERRORS) {
-//     return action.errors;
-//   }
-//   return state;
-// };
