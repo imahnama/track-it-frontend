d59ecb4c1f7aade593d333360c55a107b85d01a3
imahnama <muannama25@gmail.com>
2021-02-08 11:37:35 +0300

    Fix width of form in mobile view

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -37,12 +37,12 @@ const Login = props => {
       <Form className="w-25 m-auto pt-5" onSubmit={saveData}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+          <Form.Control type="email" className="field" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+          <Form.Control type="password" className="field" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
         </Form.Group>
         <p>{ errorMessage || '' }</p>
         <Button variant="primary" type="submit">
--- src/components/Auth/signup.js
+++ src/components/Auth/signup.js
@@ -38,17 +38,17 @@ const Signup = props => {
       <Form className="w-25 m-auto pt-5" onSubmit={submitData}>
         <Form.Group controlId="formBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter name" value={name} onChange={e => setName(e.target.value)} />
+          <Form.Control type="text" className="field" placeholder="Enter name" value={name} onChange={e => setName(e.target.value)} />
         </Form.Group>
 
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+          <Form.Control type="email" className="field" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+          <Form.Control type="password" className="field" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
         </Form.Group>
         <p>{ errorMessage || '' }</p>
         <Button variant="primary" type="submit">
